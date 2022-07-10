2b5e2c8509b013d17b153bf9623f2c3f5fd6bd2b
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-07-09 23:20:41 -0500

    Create data.js

---
+++ js/data/data.js
@@ -0,0 +1,16 @@
+const data = {
+
+    "productos" : [
+        {
+            "nombre" : "oscar",
+            "precio" : "1000",
+            "cantidad" : "12"
+        },
+        {
+            "nombre" : "maria",
+            "precio" : "2000",
+            "cantidad" : "24"
+        }
+    ]
+
+};
