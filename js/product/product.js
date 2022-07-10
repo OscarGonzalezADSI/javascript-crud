90fa47d37216dfb666f7dafd62a295663de97c3d
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-07-09 23:31:23 -0500

    Create product.js

---
+++ js/product/product.js
@@ -0,0 +1,33 @@
+class Product
+{
+    
+    setNombre(nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    getNombre()
+    {
+        return this.nombre;
+    }
+    
+    setPrecio(precio)
+    {
+        this.precio = precio;
+    }
+    
+    getPrecio()
+    {
+        return this.precio;
+    }
+    
+    setCantidad(cantidad)
+    {
+        this.cantidad = cantidad;
+    }
+    
+    getCantidad()
+    {
+        return this.cantidad;
+    }
+}
